fd4ec33f17a770a24515a591f9be941d1d08e65e
wannabegooddevv <daffa.kamal@mail.ugm.ac.id>
2023-05-01 10:57:13 +0700

    feat: add conditional styling to button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -17,13 +17,28 @@ const Button: React.FC<ButtonProps> = ({
   disabled,
   outline,
   small,
-  icon,
+  icon: Icon,
 }) => {
   return (
     <button
-      className={
-        "w-full relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition"
-      }
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+      relative
+      disabled:opacity-70
+      disabled:cursor-not-allowed
+      rounded-lg
+      hover:opacity-80
+      transition
+      w-full
+      ${outline ? "bg-white" : "bg-rose-500"}
+      ${outline ? "border-black" : "border-rose-500"}
+      ${outline ? "text-black" : "text-white"}
+      ${small ? "text-sm" : "text-md"}
+      ${small ? "py-1" : "py-3"}
+      ${small ? "font-light" : "font-semibold"}
+      ${small ? "border-[1px]" : "border-2"}
+    `}
     >
       {label}
     </button>
