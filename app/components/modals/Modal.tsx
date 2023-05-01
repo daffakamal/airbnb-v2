a222facdc0a5bafbefa64327b2a43db92d72445a
wannabegooddevv <daffa.kamal@mail.ugm.ac.id>
2023-05-01 10:42:34 +0700

    feat: add logical and conditional operator

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
   actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;
-  secondaryLabel?: string;
+  secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -26,7 +26,7 @@ const Modal: React.FC<ModalProps> = ({
   footer,
   actionLabel,
   disabled,
-  secondaryLabel,
+  secondaryActionLabel,
   secondaryAction,
 }) => {
   const [showModal, setShowModal] = useState(isOpen);
@@ -39,7 +39,6 @@ const Modal: React.FC<ModalProps> = ({
     if (disabled) {
       return;
     }
-
     setShowModal(false);
     setTimeout(() => {
       onClose();
@@ -50,14 +49,13 @@ const Modal: React.FC<ModalProps> = ({
     if (disabled) {
       return;
     }
-
     onSubmit();
   }, [disabled, onSubmit]);
+  
   const handleSecondaryAction = useCallback(() => {
     if (disabled || !secondaryAction) {
       return;
     }
-
     secondaryAction();
   }, [disabled, secondaryAction]);
 
@@ -69,9 +67,17 @@ const Modal: React.FC<ModalProps> = ({
     <>
       <div className="flex justify-center items-center overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
         <div className="relative w-full h-full lg:h-auto md:h-auto md:w-4/6 lg:w-3/6 xl:w-2/5 mx-auto my-6">
-          <div className="translate duration-300 h-full">
+          <div
+            className={`
+            translate
+            duration-300
+            h-full
+            ${showModal ? "translate-y-0" : "translate-y-full"}
+            ${showModal ? "opacity-100" : "opacity-0"}
+          `}
+          >
             <div className="w-full h-full md:h-auto relative flex flex-col bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
-              <div className="flex items-center justify-center relative border-b-[1px] p-6 rounded-t ">
+              <div className="flex items-center justify-center relative border-b-[1px] p-6 rounded-t">
                 <button
                   onClick={handleClose}
                   className="p-1 border-0 hover:opacity-70 absolute transition left-9"
@@ -83,12 +89,21 @@ const Modal: React.FC<ModalProps> = ({
               <div className="relative flex-auto p-6">{body}</div>
               <div className="flex flex-col gap-2 p-6">
                 <div className="w-full flex flex-row items-center gap-4">
+                  {secondaryAction && secondaryActionLabel && (
+                    <Button
+                      disabled={disabled}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                      outline
+                    />
+                  )}
                   <Button
                     disabled={disabled}
                     label={actionLabel}
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
