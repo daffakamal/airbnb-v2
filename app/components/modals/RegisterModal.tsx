27138b49ce88e8bc9959673cf2f4299ad7610e67
wannabegooddevv <daffa.kamal@mail.ugm.ac.id>
2023-05-04 15:28:04 +0700

    feat(modals): add onSubmit function and register functionality

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,9 +6,10 @@ import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "../hooks/useRegisterModal";
+import Modal from "./Modal";
 
 const RegisterModal = () => {
-  const RegisterModal = useRegisterModal();
+  const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
   const {
     register,
@@ -22,8 +23,30 @@ const RegisterModal = () => {
     },
   });
 
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios.post('/api/register', data)
+    .then(() => {
+      registerModal.onClose();
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    })
+  }
+
   return (
-    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4"></div>
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
   );
 };
 
